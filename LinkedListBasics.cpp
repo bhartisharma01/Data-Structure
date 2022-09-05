fda605ee429bf7f0e10f79447eebc734c853c6bc
bhartisharma01 <20001001902bharti@dcrustm.org>
2022-09-05 07:03:28 +0530

    Deletion at head, last, pos in Linked List

--- LinkedListBasics.cpp
+++ LinkedListBasics.cpp
@@ -12,6 +12,16 @@ public:
         this->data = data;
         this->next = NULL;
     }
+    ~Node()
+    {
+        int value = this->data;
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << "Memory free for the value " << value << endl;
+    }
 };
 
 void insertAtHead(Node *&head, int data)
@@ -28,28 +38,59 @@ void insertAtTail(Node *&tail, int data)
     tail = temp;
 }
 
-void insertAtPosition(Node* &tail, Node* &head, int pos, int data){
-    Node* temp = head;
-    Node* nodeToInsert = new Node(data);
-    int count =1;
-    if(pos == 1){
+void insertAtPosition(Node *&tail, Node *&head, int pos, int data)
+{
+    Node *temp = head;
+    Node *nodeToInsert = new Node(data);
+    int count = 1;
+    if (pos == 1)
+    {
         insertAtHead(head, data);
         return;
     }
 
-    if(temp -> next == NULL){
+    if (temp->next == NULL)
+    {
         insertAtTail(tail, data);
         return;
     }
-    while(count < pos-1){
-        temp = temp ->next;
+    while (count < pos - 1)
+    {
+        temp = temp->next;
         count++;
     }
 
-    nodeToInsert ->  next = temp -> next;
-    temp -> next = nodeToInsert;
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
 
-
+void deleteNode(int position, Node *&head)
+{
+    Node *prev = head;
+    Node *curr = head;
+    int cnt = 1;
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        Node *curr = head;
+        Node *prev = NULL;
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        prev->next = curr->next;
+        curr -> next = NULL;
+        delete curr;
+    }
 }
 void printLinkedList(Node *&head)
 {
@@ -71,14 +112,17 @@ int main()
     insertAtHead(head, 12);
     insertAtHead(head, 15);
     printLinkedList(head);
-    cout<<endl;
+    cout << endl;
     cout << "Insert at end" << endl;
     insertAtTail(tail, 120);
     insertAtTail(tail, 150);
     printLinkedList(head);
-cout<<endl;
-    cout<<"Inserting at particular position"<<endl;
+    cout << endl;
+    cout << "Inserting at particular position" << endl;
     insertAtPosition(tail, head, 6, 22);
+    printLinkedList(head);
+    cout<<endl;
+    deleteNode(4, head);
     printLinkedList(head);
     return 0;
 }
